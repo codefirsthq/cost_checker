78c3fac0087dfd43467c00bdf514075cc086248c
kopianan <ananalfredcarlos@gmail.com>
2021-07-15 17:20:37 +0700

    solving destination page

--- lib/presentation/destination_city/destination_city_page.dart
+++ lib/presentation/destination_city/destination_city_page.dart
@@ -5,6 +5,7 @@ import 'package:cost_checker/domain/core/status_data_model.dart';
 import 'package:cost_checker/domain/province/province_data_model.dart';
 import 'package:cost_checker/presentation/weight/weight_page.dart';
 import 'package:cost_checker/presentation/widgets/location_drop_down_button.dart';
+import 'package:cost_checker/presentation/widgets/page_title_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
@@ -18,7 +19,8 @@ class DestinationCityPage extends StatefulWidget {
 }
 
 class _DestinationCityPageState extends State<DestinationCityPage> {
-  late CityDataModel _selectedCity;
+  CityDataModel? _selectedCity;
+  final formKey = GlobalKey<FormState>();
 
   final provinceBLoc = RajaongkirBloc();
   final cityBloc = RajaongkirBloc();
@@ -36,117 +38,137 @@ class _DestinationCityPageState extends State<DestinationCityPage> {
       body: Column(
         children: [
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  padding:
-                      EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                  child: Text(
-                    "Destination City",
-                    style: TextStyle(
-                      fontSize: 40,
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  children: [
+                    PageTitle(title: "DESTINATION CITY"),
+                    SizedBox(height: 20),
+                    Container(
+                      alignment: Alignment.center,
+                      height: 150,
+                      child: Image.asset('assets/images/cf_send.png'),
                     ),
-                  ),
+                    SizedBox(height: 20),
+                    Center(
+                        child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        "Please select the city of destination to check the shipping cost",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                    )),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Form(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      key: formKey,
+                      child: Column(
+                        children: [
+                          BlocProvider(
+                            create: (context) => provinceBLoc
+                              ..add(RajaongkirEvent.getProvinceListData()),
+                            child:
+                                BlocConsumer<RajaongkirBloc, RajaongkirState>(
+                              listener: (context, state) {},
+                              builder: (context, state) {
+                                return state.maybeMap(
+                                    orElse: () => SizedBox(),
+                                    onLoading: (e) => loadingDropdownButton(),
+                                    onError: (e) => errorDropdown(e.status),
+                                    onGetProvinceData: (e) =>
+                                        LocationDropDownButton<
+                                                ProvinceDataModel>(
+                                            label: "Province",
+                                            validator: (val) {
+                                              if (val == null) {
+                                                return "Tidak ada provinsi yang dipilih";
+                                              } else {
+                                                return null;
+                                              }
+                                            },
+                                            itemData: e
+                                                .provinceResponse.results!
+                                                .map((e) => DropdownMenuItem(
+                                                      child: Text(e.province!),
+                                                      value: e,
+                                                    ))
+                                                .toList(),
+                                            onChanged: (e) {
+                                              cityBloc.add(RajaongkirEvent
+                                                  .getCityByProvinceId(
+                                                      provinceId:
+                                                          e.provinceId!));
+                                            },
+                                            hintText: "Choose City"));
+                              },
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          BlocProvider(
+                            create: (context) => cityBloc,
+                            child:
+                                BlocConsumer<RajaongkirBloc, RajaongkirState>(
+                              listener: (context, state) {},
+                              builder: (context, state) {
+                                return state.maybeMap(
+                                    orElse: () {
+                                      return SizedBox();
+                                    },
+                                    onLoading: (e) => loadingDropdownButton(),
+                                    onError: (e) => errorDropdown(e.status),
+                                    onGetCityByProvince: (e) {
+                                      return LocationDropDownButton<
+                                              CityDataModel>(
+                                          label: "City",
+                                          validator: (val) {
+                                            if (_selectedCity == null) {
+                                              return "City must not be null ";
+                                            }
+                                          },
+                                          itemData: e.cityResponse.results!
+                                              .map((e) => DropdownMenuItem(
+                                                    child: Text(e.type! +
+                                                        " " +
+                                                        e.cityName!),
+                                                    value: e,
+                                                  ))
+                                              .toList(),
+                                          onChanged: (e) {
+                                            _selectedCity = e;
+                                          },
+                                          hintText: "Choose City");
+                                    });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 20),
-                Container(
-                  alignment: Alignment.center,
-                  height: 200,
-                  child: Image.asset('assets/images/cf_send.png'),
-                ),
-                SizedBox(height: 20),
-                Center(
-                    child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Text(
-                    "Please select the city of destination to check the shipping cost",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 18, color: Colors.grey),
-                  ),
-                )),
-                SizedBox(
-                  height: 20,
-                ),
-                BlocProvider(
-                  create: (context) => provinceBLoc
-                    ..add(RajaongkirEvent.getProvinceListData()),
-                  child: BlocConsumer<RajaongkirBloc, RajaongkirState>(
-                    listener: (context, state) {},
-                    builder: (context, state) {
-                      return state.maybeMap(
-                          orElse: () => SizedBox(),
-                          onLoading: (e) => loadingDropdownButton(),
-                          onError: (e) => errorDropdown(e.status),
-                          onGetProvinceData: (e) =>
-                              LocationDropDownButton<ProvinceDataModel>(
-                                  label: "Province",
-                                  itemData: e.provinceResponse.results!
-                                      .map((e) => DropdownMenuItem(
-                                            child: Text(e.province!),
-                                            value: e,
-                                          ))
-                                      .toList(),
-                                  onChanged: (e) {
-                                    cityBloc.add(
-                                        RajaongkirEvent.getCityByProvinceId(
-                                            provinceId: e.provinceId!));
-                                  },
-                                  hintText: "Choose City"));
-                    },
-                  ),
-                ),
-                SizedBox(height: 20),
-                BlocProvider(
-                  create: (context) => cityBloc,
-                  child: BlocConsumer<RajaongkirBloc, RajaongkirState>(
-                    listener: (context, state) {},
-                    builder: (context, state) {
-                      return state.maybeMap(
-                          orElse: () {
-                            return SizedBox();
-                          },
-                          onLoading: (e) => loadingDropdownButton(),
-                          onError: (e) => errorDropdown(e.status),
-                          onGetCityByProvince: (e) {
-                            return LocationDropDownButton<CityDataModel>(
-                                label: "City",
-                                itemData: e.cityResponse.results!
-                                    .map((e) => DropdownMenuItem(
-                                          child: Text(
-                                              e.type! + " " + e.cityName!),
-                                          value: e,
-                                        ))
-                                    .toList(),
-                                onChanged: (e) {
-                                  _selectedCity = e;
-                                },
-                                hintText: "Choose City");
-                          });
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-              ],
-            ),
               ),
             ),
           ),
-          Container(
-              width: double.infinity,
-              height: 45,
-              child: ElevatedButton(
-                child: Text("SET PACKAGE WEIGHT"),
-                onPressed: () {
-                  Get.put(LocationController())
-                      .setDestinationCity(_selectedCity);
-                  Get.toNamed(WeightPage.TAG);
-                },
-              ))
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20),
+            child: Container(
+                width: double.infinity,
+                height: 45,
+                child: ElevatedButton(
+                  child: Text("SET PACKAGE WEIGHT"),
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      Get.put(LocationController())
+                          .setDestinationCity(_selectedCity!);
+                      Get.toNamed(WeightPage.TAG);
+                    }
+                  },
+                )),
+          )
         ],
       ),
     );
